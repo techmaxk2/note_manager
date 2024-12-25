b30f7822f0f69aa055058148462d242d8b16c37f
techmaxk2 <47863792+techmaxk2@users.noreply.github.com>
2024-12-25 13:29:45 +0300

    Грид 1 Задание 3

--- add_list.py
+++ add_list.py
@@ -3,13 +3,10 @@ content = input("Описание заметки: ")
 status = input("Статус заметки: ")
 created_date = input("Введите дату создания заметки в формате 'день-месяц-год: ") #"день-месяц-год", например "10-11-2024"
 issue_date = input("Введите дату истечения заметки в формате 'день-месяц-год : ") #"день-месяц-год", например "10-12-2024"
-
 # titles = []
 # for i in range(3):
 #     title = input(f"Введите заголовок заметки {i + 1}: ")
 #     titles.append(title)
-
-
 title1 = input("Введите первый заголовок: ")
 title2 = input("Введите второй заголовок: ")
 title3 = input("Введите третий заголовок: ")
@@ -17,14 +14,10 @@ notes_titles = []
 notes_titles.append(title1)
 notes_titles.append(title2)
 notes_titles.append(title3)
-
-
-
-
 print("Вы ввели следующие данные:")
 print("Имя пользователя:", username)
-print("Список заголовков ваших заметок: " , notes_titles)
+print("Список заголовков ваших заметок: ", notes_titles)
 print("Описание заметки:", content)
 print("Статус заметки:", status)
-print("Дата создания заметки:" , created_date[0:5])
-print("Дата истечения заметки:" , created_date[0:5])
+print("Дата создания заметки:", created_date[0:5])
+print("Дата истечения заметки:", created_date[0:5])
