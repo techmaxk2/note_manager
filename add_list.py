50760aa70a947071a2431dcadff2ca88d136adcd
techmaxk2 <47863792+techmaxk2@users.noreply.github.com>
2024-12-25 13:12:04 +0300

    Грид 1 Задание 4

--- add_list.py
+++ add_list.py
@@ -4,14 +4,26 @@ status = input("Статус заметки: ")
 created_date = input("Введите дату создания заметки в формате 'день-месяц-год: ") #"день-месяц-год", например "10-11-2024"
 issue_date = input("Введите дату истечения заметки в формате 'день-месяц-год : ") #"день-месяц-год", например "10-12-2024"
 
-titles = []
-for i in range(3):
-    title = input(f"Введите заголовок заметки {i + 1}: ")
-    titles.append(title)
+# titles = []
+# for i in range(3):
+#     title = input(f"Введите заголовок заметки {i + 1}: ")
+#     titles.append(title)
+
+
+title1 = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+notes_titles = []
+notes_titles.append(title1)
+notes_titles.append(title2)
+notes_titles.append(title3)
+
+
+
 
 print("Вы ввели следующие данные:")
 print("Имя пользователя:", username)
-print("Заголовки заметки:", titles)
+print("Список заголовков ваших заметок: " , notes_titles)
 print("Описание заметки:", content)
 print("Статус заметки:", status)
 print("Дата создания заметки:" , created_date[0:5])
