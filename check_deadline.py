1ed7eaf318143fe5bb37259be2e27ce8d18459af
techmaxk2 <47863792+techmaxk2@users.noreply.github.com>
2025-01-28 09:37:34 +0300

    Grade 1. Этап 2: Задание 3

--- check_deadline.py
+++ check_deadline.py
@@ -1,29 +1,23 @@
 from datetime import datetime
-
 def validate_date(input_date):
     try:
         date = datetime.strptime(input_date, '%d-%m-%Y')
         return date.date()
     except ValueError:
         return None
-
 def check_deadline():
     current_date = datetime.now().date()
-
     while True:
         issue_date_input = input("Введите дату дедлайна в формате 'день-месяц-год': ")
         issue_date = validate_date(issue_date_input)
-
         if issue_date:
             break
         else:
             print("Неверный формат даты. Попробуйте еще раз.")
-
     if issue_date < current_date:
         print(f"Дедлайн истёк {current_date - issue_date} дней назад.")
     elif issue_date > current_date:
         print(f"До дедлайна осталось {issue_date - current_date} дней.")
     else:
         print("Сегодня последний день для выполнения задания!")
-
 check_deadline()
